b1bd9d795b70e7e12b3d5165fc1085a09907418b
seam <honghaitzz11@gmail.com>
2018-03-25 17:06:34 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http = require('http');
+var $http = require('http');
 var router = express.Router();
 // 获取数据
 var URL = require('url');
@@ -40,7 +40,7 @@ router.get('/info', function(req, res, next) {
   console.log(params);
 
 	let info = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=${params.code}&grant_type=authorization_code`;
-		 http.get(info, function(ress) {
+		 $http.get(info, function(ress) {
 		    console.log(ress);
 		    console.log(ress.text);
 		    _res.redirect('/')
