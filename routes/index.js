2628d979f280df413a16523b7774d5107403db8e
seam <honghaitzz11@gmail.com>
2018-03-25 17:52:55 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const https = require('https');
+var querystring = require('querystring');
 var axios = require('axios');
 var router = express.Router();
 // 获取数据
@@ -39,7 +40,7 @@ router.get('/info', function(req, res, next) {
   let $res = res;
 	console.log('/info');
   console.log(params);
-	let info = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=${params.code}&grant_type=authorization_code`;
+	// let info = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=${params.code}&grant_type=authorization_code`;
 
 	/*axios.get(info).then(res=>{
 		console.log(res);
@@ -48,7 +49,7 @@ router.get('/info', function(req, res, next) {
   })*/
 
 
-	https.post(info, (res) => {
+	/*https.post(info, (res) => {
 		console.log(res)
 	  // console.log('statusCode:', res.statusCode);
 	  // console.log('headers:', res.headers);
@@ -59,8 +60,8 @@ router.get('/info', function(req, res, next) {
 
 	}).on('error', (e) => {
 	  console.error(e);
-	});
-
+	});*/
+  PostCode(params)
 	setTimeout(()=>{
 		$res.redirect('/')
 	},200)
@@ -78,6 +79,44 @@ router.get('/user',function(req,res,next){
 	console.log(params);
 	console.log(params.code);
 })
+
+
+
+function PostCode(params) {
+  // Build the post string from an object
+  var post_data = querystring.stringify({
+      'appid' : 'wxd1271417044b9709',
+      'secret': '07f11a1a4aa8b4279daf9d21884916e8',
+      'code': params.code,
+      'grant_type':'authorization_code'
+  });
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'https://api.weixin.qq.com/sns/oauth2/access_token',
+      port: '80',
+      // path: '/compile',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+      	console.log(chunk);
+        console.log('Response: ' + chunk);
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+}
+
 
 
 module.exports = router;
