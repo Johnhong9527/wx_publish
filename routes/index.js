9f069cdee2f84336f2f56f580fa9073de6e0ea5c
seam <honghaitzz11@gmail.com>
2018-03-25 17:00:38 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 // 获取数据
 var URL = require('url');
 /* GET home page. */
 
 
+router.get('/',function(req,res,next){
+	let params = URL.parse(req.url, true).query;
+	// console.log(params.echostr);
+	// console.log(123);
+	// res.send(params.echostr);
+	// res.send(params);
+	// let info = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=CODE&grant_type=authorization_code';
+	// console.log(params);
+	// console.log(params.code);
+	res.render('index', { title: 'Express' });
+})
+
+
 router.get('/token',function(req,res,next){
 	let params = URL.parse(req.url, true).query;
 	console.log(params.echostr);
-	console.log(123);
+	console.log('/token');
 	res.send(params.echostr);
 	// res.send(params);
 	// let info = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=CODE&grant_type=authorization_code';
@@ -21,17 +35,20 @@ router.get('/token',function(req,res,next){
 // WXtocken
 router.get('/info', function(req, res, next) {
   let params = URL.parse(req.url, true).query;
-  // console.log(req.url);
+	let _res = res;
+	console.log('/info');
   console.log(params);
-  // console.log(params.signature);
-  // console.log(params.timestamp);
-  // console.log(params.nonce);
-  // console.log(params.echostr);
-	console.log('123')
-  let _path = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd1271417044b9709&redirect_uri=https://www.5fbc5ec3.ngrok.io/&response_type=code&scope=snsapi_userinfo&state=honghaitao#wechat_redirect'
-  // res.send(params.echostr);
+
+	let info = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=${params.code}&grant_type=authorization_code`;
+		 http.get(info, function(ress) {
+		    console.log(ress);
+		    console.log(ress.text);
+		    _res.redirect('/')
+		  }).on('error', function(e) {
+		    console.log(e.message);   
+		  });
   // res.location(_path);
-  // res.redirect(_path)
+  
   // res.render('index', { title: 'Express' });
 });
 
@@ -39,7 +56,7 @@ router.get('/info', function(req, res, next) {
 router.get('/user',function(req,res,next){
 	let params = URL.parse(req.url, true).query;
 	res.send(params);
-	let info = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd1271417044b9709&secret=07f11a1a4aa8b4279daf9d21884916e8&code=CODE&grant_type=authorization_code';
+
 	console.log(params);
 	console.log(params.code);
 })
